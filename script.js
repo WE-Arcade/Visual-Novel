6a7ec15ddcf112a6c2b2301d685a4ed424c5412e
srikruthi kutumbaka <srikruthi.ksk@gmail.com>
2025-03-08 22:34:29 +0530

    changed landing2 to home and games in new tab

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function updateProgressBar() {
 function goToNextPage() {
   if (currentIndex < pages.length - 1) {
     localStorage.setItem("progress", currentIndex + 1); // Save progress
-    window.location.href = pages[currentIndex + 1];
+    window.open(pages[currentIndex + 1], "_blank");
   }
 }
 
@@ -110,13 +110,13 @@ function goToNextPage() {
 function goToPrevPage() {
   if (currentIndex > 0) {
     localStorage.setItem("progress", currentIndex - 1); // Save progress
-    window.location.href = pages[currentIndex - 1];
+    window.open(pages[currentIndex - 1], "_blank");
   }
 }
 
 function navigateTo(page) {
   console.log("Navigating to:", page); // Debugging log
-  window.location.href = `${page}.html`; // Ensure correct navigation
+  window.open(`${page}.html`, "_blank"); // Ensure correct navigation
 }
 
 function closeMenu() {
